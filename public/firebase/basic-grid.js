4bf735de52cf9ff8179f3d5c5481a2b45220a563
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-02-04 09:58:14 +0000

    Include details for basic grid example

--- public/firebase/basic-grid.js
+++ public/firebase/basic-grid.js
@@ -3,18 +3,30 @@ console.log('basic-grid');
 
 const numCards = 10;
 
+function getCardContent( num )
+{
+    const length = num % 10 + 1;
+
+    let misc = ""
+
+    for ( let i = 0; i < length; i ++ ) {
+        misc = misc + `${i} `;
+    }
+
+    return `Card-${num} <br/> ${misc}`;
+}
+
 function populateCards( num )
 {
     const container = document.querySelector('.basic-grid');
 
-    for ( let i = 1; i <= num; i ++ ) {
+    for ( let i = 0; i < num; i ++ ) {
         let div = document.createElement('div');
-        div.innerHTML = `Card-${i}`;
-        div.className = "card";
+        div.innerHTML = getCardContent(i);
+        div.className = "card cardN";
 
         container.appendChild( div );
     }
 }
 
 populateCards( numCards );
-
